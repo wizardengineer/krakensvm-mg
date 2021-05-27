72f980a1eb5c168da3eae3b72106f1687f262a5c
medievalghoul <yeet7976@gmail.com>
2021-05-26 19:10:16 -0700

    deleted

--- krakensvm/inc/krakensvm.hpp
+++ krakensvm/inc/krakensvm.hpp
@@ -4,7 +4,7 @@
 #define IA32_MSR_EFER_SVM_BIT        (1UL << 12)
 
 #include <stdint.h>
-#include "util.hpp"
+#include "hv_util.hpp"
 
 namespace svm
 {
@@ -16,15 +16,13 @@ namespace svm
     processor_feature_id    = 0x80000001,
     processor_feature_id_ex = 0x00000001,
     cpu_vendor_string       = 0x00000000,
-
-    // fn0000_0001_ecx_svm
     hypervisor_present_ex   = 0x80000000,
 
-    // fn8000_0001_ecx_svm
+    // fn8000_0001_ecx_svm bit
     svm_fn                  = 0x00000004,
 
-    // fn8000_0001_ebx_np
-    nest_page_ebx           = 0x00000001
+    // fn8000_0001_ebx_np bit
+    nest_page_fn            = 0x00000001
   };
 
   auto svm_support_checking() noexcept -> bool;
--- krakensvm/svm/krakensvm.cpp
+++ krakensvm/svm/krakensvm.cpp
@@ -5,7 +5,61 @@ namespace svm
 
   auto svm_support_checking() noexcept -> bool
   {
+    int32_t regs[4]      = {};
 
+    //
+    // checks to see if the processor is a AMD processor
+    //
+
+    bool processor_check =
+          [&regs]()
+          {
+            __cpuid(regs, (int)cpuid_e::cpu_vendor_string);
+            if (regs[1] == 'htuA' &&
+                regs[2] == 'DMAc' &&
+                regs[3] == 'itne') { return true; }
+            return false;
+          }();
+
+    //
+    // checks to see if the processor supports SVM
+    //
+
+    bool svm_check =
+          [&regs]()
+          {
+            __cpuid(regs, (int)cpuid_e::svm_features);
+            if ((regs[2] &(int)cpuid_e::svm_fn) == 1)
+            { return true; }
+
+            return false;
+          }();
+
+    //
+    // checks to see if the processor supports Nested Paging
+    //
+
+    bool nest_paging_check =
+          [&regs]()
+          {
+            __cpuid(regs, (int)cpuid_e::svm_features);
+            if (regs[3] & (int)cpuid_e::nest_page_fn == 1)
+            { return true; }
+
+            return false;
+          }();
+
+    //
+    // checks to see if the EFER.SVM can be enabled, if the VM_CR.SVMDIS
+    // is set then we can't enable SVM.
+    //
+    bool vmcr_check =
+          [&regs]()
+          {
+            if (__readmsr() == )
+          }();
+
+    return processor_check && svm_check;
   }
 
   auto svm_enabling()         noexcept -> void
