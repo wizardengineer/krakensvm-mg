7eaa41fc47ad5526fc76f2a74671656734692923
medievalghoul <yeet7976@gmail.com>
2021-05-27 19:20:15 -0700

    [KRKEN/INC] Re-arranged functions

--- krakensvm/inc/krakensvm.hpp
+++ krakensvm/inc/krakensvm.hpp
@@ -1,3 +1,28 @@
+/* This file is part of mg-hypervisor by medievalghoul, licensed under the MIT license:
+*
+* MIT License
+*
+* Copyright (c) medievalghoul 2021
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in all
+* copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*/
+
 #pragma once
 
 #define IA32_MSR_EFER                0xC0000080
@@ -22,9 +47,11 @@ namespace svm
     svm_fn                  = 0x00000004,
 
     // fn8000_0001_ebx_np bit
-    nest_page_fn            = 0x00000001
+    nest_page_fn            = 0x00000001,
+
+    //fn8000_0001_ecx_
   };
 
-  auto svm_support_checking() noexcept -> bool;
-  auto svm_enabling()         noexcept -> void;
+  static auto svm_support_checking() noexcept -> bool;
+  auto svm_enabling               () noexcept -> void;
 };
