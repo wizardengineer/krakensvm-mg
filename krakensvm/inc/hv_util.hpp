c5d45507630dbb2b8e928073f9057e3fbd052d71
medievalghoul <yeet7976@gmail.com>
2021-07-09 16:58:32 -0400

    [KRKEN/INC] Added more MSR's and added Virtual CPU Data

--- krakensvm/inc/hv_util.hpp
+++ krakensvm/inc/hv_util.hpp
@@ -1,4 +1,4 @@
-/* This file is part of mg-hypervisor by medievalghoul, licensed under the MIT license:
+/* This file is part of krakensvm-mg by medievalghoul, licensed under the MIT license:
 *
 * MIT License
 *
@@ -32,6 +32,19 @@
 #include <intrin.h>
 #include <basetsd.h>
 #include <ntifs.h>
+#include <utility>
+
+// HV Specific headers
+#include <vmcb.hpp>
+#include <paging.hpp>
+#include <krakensvm.hpp>
+#include <segment_intrins.h>
+#include <descriptors_info.hpp>
+
+
+
+extern "C" void _sgdt(void*);
+#pragma intrinsic(_sgdt);
 
 template<class type>
 concept integral = std::is_integral_v<type>;
@@ -46,25 +59,53 @@ inline void __cpuid(int[CPUID_MAX_REGS], int);
 inline void __cpuidex(int[CPUID_MAX_REGS], int, int);
 #pragma intrinsic(__cpuidex)
 
+//
+// VCPU Specific data & Intercept Data (Apart of the VMCB Layout, Control Area)
+//
+
+typedef union _vcpu_ctx
+{
+  __declspec(align(PAGE_SIZE)) vmcb::vmcb_64_t guest_vmcb;
+  __declspec(align(PAGE_SIZE)) vmcb::vmcb_64_t host_vmcb;
+
+  __declspec(align(PAGE_SIZE)) uint8_t host_state_area[PAGE_SIZE]; 
+
+  void* msrpm_pa; // Physical Address of "MSR Permission Maps"
+} vcpu_ctx_t, *pvcpu_ctx;
+
+
+// Vector 00CH
+#define INTERCEPT_CPUID    (1UL << 18) // Intercept CPUID Instruction.
+#define INTERCEPT_HLT      (1UL << 24) // Intercept HLT instruction.
+#define INTERCEPT_MSR_PROT (1UL << 28) // MSR_PROT—intercept RDMSR or WRMSR accesses to selected MSRs.
+#define INTERCEPT_FERR_FRE (1UL << 30) // FERR_FREEZE: intercept processor “freezing” during legacy FERR handling.
+
+// Vector 010H
+#define INTERCEPT_VMRUN    (1UL << 0)  // Intercept VMRUN instruction.
+#define INTERCEPT_EFER     (1UL << 15) // Intercept VMRUN instruction.
+
+
 // 
 // MSR
 //
 
-/*
- * The VM_CR MSR controls certain global aspects of SVM. The VM_CR.SVMDIS
- * is a bit, if this bit was set, it would disallow the act of  setting
- * EFER.SVM bit.
- */
+// The VM_CR MSR controls certain global aspects of SVM. The VM_CR.SVMDIS
+// is a bit, if this bit was set, it would disallow the act of  setting
+// EFER.SVM bit.
 
 #define vm_cr           0xC0010114
 #define vm_cr_svmdis    0x10           // 0x10 will check the 5 bit of VM_CR
 
- /*
-  * Extended Feature Enable Register (EFER)
-  */
+// Extended Feature Enable Register (EFER)
 
 #define ia32_efer       0xC0000080
-#define ia32_efer_svme  0x1000        // this will set the 13 bit of EFER 
+#define ia32_efer_svme  0x1000        // this will set the 13 bit of EFER
 
+// Virtual Machine Host Save State Physical Addres (VM_HSAVE_PA)
 
+#define vm_hsave_pa     0xC0010117
+
+// Page Attribute Table (PAT)
+
+#define ia32_pat        0x00000277
 
